c392c1f4195acd9b7b30868d1ff04a5ca4dbfde8
Jiawei Fu <fujiaweisteven@hotmail.com>
2020-07-22 21:54:51 +0800

    update Tara

---
+++ R/data1.R
@@ -0,0 +1,13 @@
+#' Sample dataset
+#'
+#' A dataset containing xxx.
+#'
+#'
+#' @format A sample data frame
+#' \describe{
+#'   \item{d}{code for each country}
+#'   \item{n_voters}{the preference for income redistribution from 1 to 10}
+#'   \item{c}{individuals' income level}
+#' }
+#'
+"rv"
