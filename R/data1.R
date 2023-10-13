211e67a4248230de1b2408b7ac75127e57e5dacd
Tara Slough <taraslough@gmail.com>
2023-10-13 15:53:42 -0400

    Update data1.R

--- R/data1.R
+++ R/data1.R
@@ -1,13 +1,13 @@
-#' Sample dataset
+#' Example dataset with clusters
 #'
-#' A dataset containing xxx.
+#' A data set with example data used in vignettes for for the function get_maei_rand. 
 #'
 #'
 #' @format A sample data frame
 #' \describe{
-#'   \item{d}{code for each country}
-#'   \item{n_voters}{the preference for income redistribution from 1 to 10}
-#'   \item{c}{individuals' income level}
+#'   \item{d}{Denotes each district}
+#'   \item{n_voters}{The number of registered voters in each cluster in each district.}
+#'   \item{c}{Denotes the clusters within each district.}
 #' }
 #'
 "rv"
